4545896c7f92d25b071122a26ded85420b36e0cc
AOA <aoa@d.co>
2025-11-07 12:37:25 +0200

    #2.0.0 Update

--- src/main/java/net/alminoris/aestheticwindows/block/custom/FourpaneEmptyWindowVerticalBlock.java
+++ src/main/java/net/alminoris/aestheticwindows/block/custom/FourpaneEmptyWindowVerticalBlock.java
@@ -6,7 +6,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -14,7 +13,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Hashtable;
-import java.util.Map;
 
 public class FourpaneEmptyWindowVerticalBlock extends BaseWindowVerticalBlock
 {
@@ -27,20 +25,13 @@ public class FourpaneEmptyWindowVerticalBlock extends BaseWindowVerticalBlock
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit)
     {
         ItemStack stack = player.getMainHandStack();
-        if (!world.isClient() && !getMaterialName().isEmpty() && isGlassBlockInStack(stack))
+        if (!world.isClient() && !getMaterialName().isEmpty() && stack.isOf(Blocks.GLASS_PANE.asItem()))
         {
             stack.decrement(1);
-
-            String colorName;
-            if (!stack.isOf(Blocks.GLASS_PANE.asItem()))
-                colorName = Registries.ITEM.getId(stack.getItem()).getPath().replace("_stained_glass_pane", "");
-            else
-                colorName = "none";
 
             world.setBlockState(pos, ModBlocks.VERTICAL_FOURPANE_WINDOWS.get(getMaterialName()).getDefaultState()
                     .with(FACING, state.get(FACING))
                     .with(VARIANT, state.get(VARIANT))
-                    .with(GLASS_COLOR, GlassColor.fromString(colorName))
                     .with(OPEN, state.get(OPEN))
                     .with(WATERLOGGED, state.get(WATERLOGGED)));
 
