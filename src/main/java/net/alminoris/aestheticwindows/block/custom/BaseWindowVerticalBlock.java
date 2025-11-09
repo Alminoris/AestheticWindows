91a40e5720d0682a683cadca2b9e1e919d11d550
AOA <aoa@d.co>
2025-11-09 08:41:02 +0200

    #2.0.0 Update

--- src/main/java/net/alminoris/aestheticwindows/block/custom/BaseWindowVerticalBlock.java
+++ src/main/java/net/alminoris/aestheticwindows/block/custom/BaseWindowVerticalBlock.java
@@ -152,7 +152,7 @@ public class BaseWindowVerticalBlock extends YAxisRotatedBlock
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit)
     {
-        if (!player.getMainHandStack().isEmpty() || state.get(VARIANT) == Variant.CENTER)
+        if (!player.getMainHandStack().isEmpty())
         {
             return ActionResult.PASS;
         }
